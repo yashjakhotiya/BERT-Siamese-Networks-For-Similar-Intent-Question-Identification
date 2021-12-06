be6541db50c0df729762e765852198ebe26b1e8c
Yash <mailsforyashj@gmail.com>
2021-12-06 06:20:33 -0500

    Update code files with final results

--- paths.py
+++
@@ -1,6 +0,0 @@
-import os
-cwd = os.getcwd()
-train_path = os.path.join(cwd, 'train.csv')
-test_path = os.path.join(cwd, 'test.csv')
-embeddings_path = os.path.join(cwd, 'embeddings.pkl')
-glove_path = os.path.join(cwd, 'glove.6B.100d.txt')
